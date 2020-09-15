82aafd99a4db4ec81f8d11c003d0e105c4408e15
jefersonscarpi <jefersonscarpi@gmail.com>
2020-09-14 22:43:25 -0300

    Projeto para Sortea numeros aleatórios de 0 a 10

---
+++ Sorteirodenumeros/app/src/main/java/com/example/sorteirodenumeros/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.sorteirodenumeros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view){
+
+        TextView texto = findViewById(R.id.textoExibicao);
+        int x = new Random().nextInt(11);
+        texto.setText("Numero sortiado " + x);
+
+
+
+    }
+
+}
